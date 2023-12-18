85e5ef820b3bb852fbf4064c641d1ea593da8ce9
usernamecsh <2941553974@qq.com>
2023-12-18 20:38:55 +0800

    feat:项目新增基础配置

---
+++ src/views/AboutView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about-page">about</div>
+</template>
+
+<script setup lang="ts"></script>
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="home-page">home</div>
+</template>
+
+<script setup lang="ts"></script>
+<style lang="scss" scoped></style>
